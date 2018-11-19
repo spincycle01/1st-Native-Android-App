f509b060337184ea758f45159f9330a612f1aa55
Adam <spincycle01@yahoo.com>
2018-11-19 12:56:41 -0500

    added 2nd button, 2 activities

--- app/src/main/java/com/example/adamhome/msgsharingapp/MainActivity.kt
+++ app/src/main/java/com/example/adamhome/msgsharingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.adamhome.msgsharingapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //code for button response
+        //code for 1st button response
         btnShowToast.setOnClickListener {
 
             //check if working, display message to logcat
@@ -23,8 +24,26 @@ class MainActivity : AppCompatActivity() {
             //must pass the Activity it will appear in as the first parameter (this), the msg is the 2nd parameter
             //3rd parameter is the duration of the msg
             //.show() method is used for displaying
-            Toast.makeText(this, "Button was clicked !", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Page Still Under Construction", Toast.LENGTH_SHORT).show()
         }
+
+        //code for 2nd button response
+        btnSendMsgToSecondActivity.setOnClickListener {
+
+            //use edit text field, extracting data out of it and converting it to string, store in variable message
+            val message: String = etUserMessage.text.toString()
+
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+
+            //this is EXPLICIT INTENT - we know the target of our intent (Second Activity)
+            //to navigate from one activity to the other, define intent
+            //first parameter is current Activity (this), 2nd parameter is address of target activity - SecondActivity name
+            //use the concept of kotlin reflection :: class.java
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
 
